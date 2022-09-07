6fc42ca21172a003c6003df7a035b831a28e0a57
Keerti Chaudhary <17keerti@gmail.com>
2022-09-07 16:14:00 -0700

    map GeoJSON LineString type objects

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -30,7 +30,7 @@ let baseMaps = {
 let map = L.map("mapid", {
   center: [44.0, -80.0],
   zoom: 2,
-  layers: [light],
+  layers: [dark],
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -40,9 +40,28 @@ L.control.layers(baseMaps).addTo(map);
 let torontoData =
   "https://raw.githubusercontent.com/17keerti/mapping_earthquakes/main/torontoRoutes.json";
 
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2,
+};
+
 // Grabbing our GeoJSON data.
 d3.json(torontoData).then(function (data) {
   console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data).addTo(map);
+  L.geoJSON(data, {
+    style: myStyle,
+    onEachFeature: function (feature, layer) {
+      console.log(layer);
+      layer.bindPopup(
+        "<h3>" +
+          "Airline: " +
+          feature.properties.airline +
+          "</h3><hr><p> Destination: " +
+          feature.properties.dst +
+          "</p>"
+      );
+    },
+  }).addTo(map);
 });
