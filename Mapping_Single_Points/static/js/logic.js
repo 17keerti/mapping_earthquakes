36e4055e886240872a48bccb163173d917f09835
Keerti Chaudhary <17keerti@gmail.com>
2022-09-07 00:54:29 -0700

    add circle to the map

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -17,3 +17,8 @@ streets.addTo(map);
 
 //  Add a marker to the map for Los Angeles, California.
 let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Add circle to the map
+L.circle([34.0522, -118.2437], {
+  radius: 100,
+}).addTo(map);
